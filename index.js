1f7a76d44677c02930d673c7fc5414461bc4c7b5
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 11:22:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-// Capitalize the first letter of a strig
+// Capitalize the first letter of a string
 export function capitalize(str) {
-  if (!str) return 
-  return str.charAt(0).toUpperCa+str.slice(1);
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-// Convert a string to kebab
-export function kebabCase
-  if (!str) return 
-  return s
-    .replace(/\s+/g,
-    .replace(/([a-z])(A-Z])/g,"1-$2
-    .toLowerCase(
+// Convert a string to kebab-case
+export function kebabCase(str) {
+  if (!str) return "";
+  return str
+    .replace(/\s+/g, "-")
+    .replace(/([a-z])([A-Z])/g, "$1-$2")
+    .toLowerCase();
+}
 
 // Reverse a string
-export function revers(str) {
+export function reverse(str) {
   if (!str) return "";
   return str.split("").reverse().join("");
 }
