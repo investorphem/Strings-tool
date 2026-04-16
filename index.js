843821352b60482103abb31acb42bcff329e876a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 07:52:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ export function capitalize(str) {
 
 // Convert a string to kebab-cas
 export function kebabCase(str) 
-  if (!str) return "
+  if (!str) return 
   return st
     .replace(/\s+/g, "-")
     .replace(/([a-z])([A-Z])/g, "$1-$2"
